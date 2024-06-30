3ac8a091888af97839c5271a4bbd5c860c5ca7da
cncj888 <32574882+cncj888@users.noreply.github.com>
2024-06-30 12:57:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = 'XXXX';
+let userID = '93076a2e-cb92-4c84-8eeb-60f66ea26b15';
 
 const proxyIPs = [
     "8.222.128.131",
